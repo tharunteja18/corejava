d95c3a4fad63b9d5b5d3b97d276475b0ca927707
tharunteja18 <tharunteja114@gmail.com>
2022-10-24 10:38:01 +0530

    added new programs

---
+++ src/main/java/com/tharun/basics/ReserveWord.java
@@ -0,0 +1,12 @@
+package com.tharun.basics;
+
+public class ReserveWord {
+    public static void main(String[] args) {
+        int If=10;
+        System.out.println(If);
+        int integer=9;
+        System.out.println(integer);
+        int Integer=8;
+        System.out.println(Integer);
+    }
+}
---
+++ src/main/java/com/tharun/basics/mains.java
@@ -0,0 +1,20 @@
+package com.tharun.basics;
+
+class mains{
+    
+public static void main(String args[])
+    {
+//        Byte value=40;
+//        long data=9223372036854775807l;
+//        long a=10;
+//        System.out.println("byte size is::"+Byte.SIZE);
+//        System.out.println("MINVALUE  is::"+Byte.MIN_VALUE);
+//        System.out.println("MAXVALUE  is::"+Byte.MAX_VALUE);
+        int  a=10;
+        a=a+1;
+        a++;
+        a+=1;
+        System.out.println(a);
+
+    }
+}
